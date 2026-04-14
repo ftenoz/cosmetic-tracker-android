64677120d2381d51bb421a5c1ffb7eda26e32ff5
Ferit Tenöz <ftenoz@gmail.com>
2026-04-14 16:20:19 +0300

    chore: Bump version to 1.0.3 (v5) after merging registration feature

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.vela.cosmetictracker"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.2"
+        versionCode = 5
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
