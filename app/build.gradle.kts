d7ec12e1948567857063bbcc181a5119ad51cc96
Ferit Tenöz <ftenoz@gmail.com>
2026-04-20 15:06:27 +0300

    Increment versionCode to 6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.vela.cosmetictracker"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.0.3"
+        versionCode = 6
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
